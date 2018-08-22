577d3c82d85fddb50290b3bcf840c1f4f1e12199
Ben Whalley <ben.whalley@plymouth.ac.uk>
2018-08-22 14:50:20 +0100

    added plain version without logos

--- floorplan.R
+++ floorplan.R
@@ -2,9 +2,9 @@ library(png)
 library(grid)
 library(tidyverse)
 
-rooms<- read_csv("roomdata.csv")
-g <- rasterGrob(readPNG("floorplan.png"), interpolate=TRUE) 
+rooms <- read_csv("roomdata.csv")
 
+g <- rasterGrob(readPNG("floorplan.png"), interpolate=TRUE) 
 p <- ggplot(rooms, aes(x = x, y = y)) +
   xlim(0,255) + ylim(0,156) +
   annotation_custom(g, xmin = -Inf, xmax = Inf, ymin = -Inf,  ymax = Inf) +
@@ -13,6 +13,18 @@ p <- ggplot(rooms, aes(x = x, y = y)) +
   theme_void()
 
 ggsave("FindMyTutor.png", p, width=25.5, height = 15.6, units='cm')
+
+
+# added a plain version without logos/text
+g <- rasterGrob(readPNG("floorplan_plain.png"), interpolate=TRUE) 
+p <- ggplot(rooms, aes(x = x, y = y)) +
+  xlim(0,255) + ylim(0,156) +
+  annotation_custom(g, xmin = -Inf, xmax = Inf, ymin = -Inf,  ymax = Inf) +
+  geom_text(aes(label = first), hjust = 0, size = 1.6, nudge_x = 1, nudge_y = 6, na.rm = TRUE) +
+  geom_text(aes(label = last), hjust = 0, size = 1.6, nudge_x = 1, nudge_y = 4, na.rm = TRUE) +
+  theme_void()
+
+ggsave("FindMyTutor_plain.png", p, width=25.5, height = 15.6, units='cm')
 
 
 
